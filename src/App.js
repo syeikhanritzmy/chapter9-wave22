863176d9d82e2a68ecc005d60bf7fbf01922f85d
yusufginanjar <yusufginanjar16@gmail.com>
2022-08-26 23:04:12 +0700

    Init setup: folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import './App.css';
+
+import Landing from "./pages/Landing"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={< Landing />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
