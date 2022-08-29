610264310d6473372a7391db66c3ee998e843b16
syeikhanritzmy <syeikhanritzmy@gmail.com>
2022-08-30 02:15:13 +0800

    create component login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-
+import Login from './pages/login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <Routes>
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
