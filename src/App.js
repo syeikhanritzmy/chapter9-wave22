72fa4b95a436c83378551892484cb6dd792f08ef
yusufginanjar <yusufginanjar16@gmail.com>
2022-08-30 15:55:45 +0700

    Game: static page of rock-paper-scissors

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import Landing from "./pages/Landing"
 import Profile from "./pages/Profile"
 import ProfileEdit from "./pages/ProfileEdit"
+import Game from "./pages/Game"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/players/:id" element={< Profile />}/>
           <Route path="/players/edit/:id" element={< ProfileEdit />}/>
+          <Route path="/game/rps" element={< Game />}/>
           <Route path="/" element={< Landing />}/>
         </Routes>
       </Router>
