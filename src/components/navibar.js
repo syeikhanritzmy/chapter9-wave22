50016ffa27d7eb355b5e2d71f43d340e2e991afc
yusufginanjar <yusufginanjar16@gmail.com>
2022-09-06 18:49:52 +0700

    Navibar Log Out and My Profile

--- src/components/navibar.js
+++ src/components/navibar.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react'
+import { getAuth, onAuthStateChanged  } from "firebase/auth";
 import Container from "react-bootstrap/Container";
+import { signOut } from 'firebase/auth';
 import { Nav } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import "../App.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navibar() {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      }
+    })
+  }, [auth]);
+
+  const handleSignOut = async () => {
+    try {
+      console.log("signing out");
+      await signOut(auth);
+      navigate('/login');
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <div>
       <Navbar bg="dark" variant="dark" className="Navbar">
@@ -29,14 +53,25 @@ export default function Navibar() {
               About
             </Nav.Link>
           </Nav>
+            { user ? 
+          <Nav className="justify-content-end">
+            <Nav.Link as={Link} to={`/players/${user.uid}`}>
+              My Profile
+            </Nav.Link>
+            <Nav.Link onClick={handleSignOut}>
+              Log Out
+            </Nav.Link>
+          </Nav>
+              : 
           <Nav className="justify-content-end">
             <Nav.Link as={Link} to={"/login"}>
               Login
             </Nav.Link>
-            <Nav.Link as={Link} to="#">
+            <Nav.Link as={Link} to="/register">
               Sign Up
             </Nav.Link>
           </Nav>
+            }
         </Container>
       </Navbar>
     </div>
