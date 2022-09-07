4c78838dc260fe96186b8ce71dce07ee21c3f8bb
yusufginanjar <yusufginanjar16@gmail.com>
2022-09-07 11:18:53 +0700

    dev top score: static page

--- src/pages/Rank.jsx
+++ src/pages/Rank.jsx
@@ -22,74 +22,83 @@ export default function Rank() {
             })
         });
     }, [])
-
-
-    // function perbandinganSkor(key, order = 'asc') {
-
-    //     return function innerSort(a, b) {
-
-    //         if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-
-    //         // property doesn't exist on either object
-
-    //         return 0;
-
-    //         }
-
-    //         const varA = (typeof a[key] === 'string')
-
-    //         ? a[key].toUpperCase() : a[key];
-
-    //         const varB = (typeof b[key] === 'string')
-
-    //         ? b[key].toUpperCase() : b[key];
-
-    //         let comparison = 0;
-
-    //         if (varA > varB) {
-
-    //         comparison = 1;
-
-    //         } else if (varA < varB) {
-
-    //         comparison = -1;
-
-    //         }
-
-    //         return (
-
-    //         (order === 'desc') ? (comparison * -1) : comparison
-    //         );
-    //     };
-    //  }
      
     return(
-        <div className='rank-page'>
-            <div className='container h-100 d-flex justify-content-center align-items-center pt-4'>
-                <div className="card mb-3 h-80">
-                    <div className="row g-0">
-                        <div className="col-md-5">
+        <div className='rank-page bg-black'>
+            <section id="top-scores" className="top-scores" >
+                <div className="background-overlay"></div>
+                <div className="container">
+                    <div className="row">
+                    <div className="col-lg-6 pt-4">
+                        <h2 className="h1 display-4 text-white text-center">ROCK PAPER SCISSORS</h2>
+                        <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et sequi quae ipsum unde consequatur fugiat exercitationem vel ipsa commodi? Velit.</p>
+                        <div className="d-block text-center">
+                        <a href="#" className="btn btn-lg btn-block btn-warning  my-4">PLAY NOW</a>
                         </div>
-                        <div className="col-md-7 ps-4">
-                        <div className="card-body">
-                            <h1 className="card-title">Rank Leaderboard</h1>
-                            <table className="table">
-                                <tbody>
-                                    <tr>
-                                        <td>Nickname:</td>
-                                        <td>{player.username}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Total Score:</td>
-                                        <td>{player.score}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="grid-container">
+                            
+                        <div className="grid-item item1 mb-4">
+                        <h2 className="h1 text-white text-center">TOP SCORES</h2>
+                            <p className="text-center">This top score from various games provided on this polatform</p>
+                            <div className="card bg-dark mt-4">
+                            <div className="card-body">
+                                <div className="d-flex">
+                                <div className="profil-image me-4 mb-4">
+                                    <img src="" alt=""/>
+                                    <div className="back-shadow bg-warning"></div>
+                                </div>
+
+                                <h4 className="card-title mt-3 text-warning">Evan Lathi<span className="h6 text-muted"><br/>PC Gamer</span></h4>
+                                <a href="#" className="twitter-icon ms-auto mt-3"></a>
+                                </div>
+                                <p className="card-text">"One of my gaming highlights of the year."</p>
+                                <div className="text-muted">
+                                June 18, 2021
+                                </div>
+                            </div>
+                            </div>
+                            <div className="card bg-dark mt-4">
+                            <div className="card-body">
+                                <div className="d-flex">
+                                <div className="profil-image me-4 mb-4">
+                                    <img src="" alt=""/>
+                                    <div className="back-shadow bg-warning"></div>
+                                </div>
+
+                                <h4 className="card-title mt-3 text-warning">Evan Lathi<span className="h6 text-muted"><br/>PC Gamer</span></h4>
+                                <a href="#" className="twitter-icon ms-auto mt-3"></a>
+                                </div>
+                                <p className="card-text">"One of my gaming highlights of the year."</p>
+                                <div className="text-muted">
+                                June 18, 2021
+                                </div>
+                            </div>
+                            </div>
+                            <div className="card bg-dark mt-4">
+                            <div className="card-body">
+                                <div className="d-flex">
+                                <div className="profil-image me-4 mb-4">
+                                    <img src="" alt=""/>
+                                    <div className="back-shadow bg-warning"></div>
+                                </div>
+
+                                <h4 className="card-title mt-3 text-warning">Evan Lathi<span className="h6 text-muted"><br/>PC Gamer</span></h4>
+                                <a href="#" className="twitter-icon ms-auto mt-3"></a>
+                                </div>
+                                <p className="card-text">"One of my gaming highlights of the year."</p>
+                                <div className="text-muted">
+                                June 18, 2021
+                                </div>
+                            </div>
+                            </div>
                         </div>
                         </div>
                     </div>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
     )
 }
